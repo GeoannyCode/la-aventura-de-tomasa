a1f17fa8e6f13f9761649eee529db319b815a940
geovannydev <diegobracero@hotmail.com>
2023-02-01 13:09:01 -0500

    player bug fixed

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 signal player_f
 export (int) var speed = 400
+onready var _animation: =$AnimatedSprite
 var velocity = Vector2.ZERO
 var in_area = null
 var dialog = false
@@ -10,28 +11,60 @@ func get_inputs():
 	velocity = Vector2()
 	if(Input.is_action_pressed("intro")):
 		emit_signal("player_f", in_area)
-	if(Input.is_action_pressed("move_up")):
-		velocity.y -= speed 
-		$AnimatedSprite.play("up")
-	if(Input.is_action_pressed("move_down")):
-		velocity.y += speed
-		$AnimatedSprite.play("down")
-	if(Input.is_action_pressed("move_left")):
-		velocity.x -= speed
-		$AnimatedSprite.play("left")
-	if(Input.is_action_pressed("move_right")):
-		velocity.x += speed
-		$AnimatedSprite.play("right")
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	else:
-		$AnimatedSprite.stop()
+#	if(Input.is_action_pressed("move_up")):
+#		velocity.y -= speed 
+#		$AnimatedSprite.play("up")
+#	if(Input.is_action_pressed("move_down")):
+#		velocity.y += speed
+#		$AnimatedSprite.play("down")
+#	if(Input.is_action_pressed("move_left")):
+#		velocity.x -= speed
+#		$AnimatedSprite.play("left")
+#	if(Input.is_action_pressed("move_right")):
+#		velocity.x += speed
+#		$AnimatedSprite.play("right")
+#	if velocity.length() > 0:
+#		velocity = velocity.normalized() * speed
+#	else:
+#		$AnimatedSprite.stop()
 	
-func _physics_process(_delta):
-	get_inputs()
-	velocity = move_and_slide(velocity)	 
-		
+#func _physics_process(_delta):
+#	get_inputs()
+#	velocity = move_and_slide(velocity)	
 
+func _physics_process(delta):
+	var direction := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	) 
+	
+	if direction.length() == 0:
+		_animation.stop()
+	else:
+		if direction.length() > 1.0 and direction[0] == 1:
+			_animation.play("right")
+			direction = direction.normalized()
+	
+		elif direction.length() > 1.0 and direction[0] == -1:
+			_animation.play("left")
+			direction = direction.normalized()
+		else:
+			if direction[1] == 1:
+				_animation.play("down")
+			if direction[1] == -1:
+				_animation.play("up")
+			if direction[0] == 1:
+				_animation.play("right")
+			if direction[0] == -1:
+				_animation.play("left")		
+	move_and_slide(speed * direction)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("intro"):
+		emit_signal("player_f", in_area)
+		print("intro")
+	
+		
 func _on_player_area_entered(area):
 	if area.is_in_group("steps_down"):
 		in_area = area
