66e525bd6a83b88774c17133eaed6059fa96f8b2
geovannydev <diegobracero@hotmail.com>
2023-02-11 16:42:09 -0500

    add dialogs

--- Scripts/dialogUI.gd
+++ Scripts/dialogUI.gd
@@ -19,8 +19,9 @@ const IMG: Dictionary = {
 }
 
 const TEXT_CHEST: Array =[
-	'TOMASA: Que cofre tan extraño solo se puede abrir con 3 llaves',
-	'TOMASA: Quiza mi madre sepa algo',
+	'TOMASA: Qué cofre tan extraño...',
+	'TOMASA: Solo se puede abrir con 3 llaves',
+	'TOMASA: Quizá  mi madre sepa algo...',
 ]
 
 const TEXT_CHEST2: Array = [
@@ -29,58 +30,114 @@ const TEXT_CHEST2: Array = [
 ]
 
 const TEXT_FOOD: Array =[
-	'TOMASA: Texto de prueba linea 1',
-	'TOMASA: Texto de prueba linea 2',
+	'TOMASA: Ingredientes del encocado de pescado que tanto me gusta...',
+	'TOMASA: Pescado, dientes de ajo, cebolla paiteña, leche de coco...',
+	'TOMASA: Limones grandes, sal pimienta y comino al gusto todo está aquí.',
 ]
 
 const TEXT_MOM: Array =[
-	'MOM: Texto de prueba linea 1',
-	'MOM: Texto de prueba linea 2',
+	'Carmelina: ¿El cofre?, si es un regalo que te dejó tu abuelo Salomón...',
+	'Carmelina: Verás tu abuelo nació en Carpue-la Valle del Chota...',
+	'Carmelina: Y es parte del grupo delos cofrades...',
+	'Carmelina: Ellos se encargan de organizar la festividad de la Semana Santa...',
+	'Carmelina: También es buen músico de la bomba...',
+	'Carmelina: Me dijo que las llaves las tienen vecinos de la comunidad...',
+	'Carmelina: Puedes ir a preguntarles,  pero no te elejes mucho del pueblo.',
 ]
 
 const TEXT_TOOLS: Array =[
-	'TOMASA: Texto de prueba linea 1',
-	'TOMASA: Texto de prueba linea 2',
+	'TOMASA: La mesa de herramientas de don Juan...',
+	'TOMASA: Debe estar detrás de su casa con sus plantas.',
 ]
 
 const TEXT_JUAN: Array =[
-	'JUAN: Texto de prueba linea 1',
-	'JUAN: Texto de prueba linea 2',
+	'JUAN: En mi huertita tengo plantitas, para curarme la barriguita...',
+	'JUAN: cuando me enfermo, mi abuelita siempre prepara ricas agüitas...',
+	'JUAN: así lo dice mi abuelita, pronto prontito te sanarás...',
+	'JUAN: Dolor de barriga, la manzanilla. Dolor de riñón, tres gotas de limón.',
+	'JUAN: Dolor de cabeza, y se curó Doña Teresa..',
+	'JUAN: ¿Sabías que...? Hay muchas plantas que puedes sembrar en tu casa... ',
+	'JUAN: Plantas como manzanilla, hierba luisa, orégano, menta, hierba buena, etc...',
+	'JUAN: ¿La llave? Si la perdí en el bosque que está justo al frente de mi casa.',
+	'JUAN: Puedes ir a buscarla, debe seguir ahí',
 ]
 
 const TEXT_PEDRO: Array =[
-	'PEDRO: Texto de prueba linea 1',
-	'PEDRO: Texto de prueba linea 2',
+	'PEDRO: ¿Sabías que...?',
+	'PEDRO: El lugar donde nacieron nuestros padres, ancestros/as...',
+	'PEDRO: y todas nuestras familias...',
+	'PEDRO: Es donde vivieron nuestros antiguos abuelos/as...',
+	'PEDRO: Que vinieron de África a trabajar producto de la esclavización...',
+	'PEDRO: Ellos cuidaron y defendieron estas tierras.',
+	'PEDRO: Aquí abajo vive SOFIA con sus vacas, ella debe tener una llave...',
+	'PEDRO: Busca una entrada de arco de piedra',
 ]
 
 const TEXT_ALEX: Array =[
-	'ALEX: Texto de prueba linea 1',
-	'ALEX: Texto de prueba linea 2',
+	'ALEX: ¿Sabías que...?',
+	'ALEX: La especie humana se originó en el continente africano...',
+	'ALEX: En la región geográfica conocida como África subsahariana...',
+	'ALEX: De esta misma región, millones de años después...',
+	'ALEX: Provienen los ancestros y las ancestras de la población...',
+	'ALEX: afroecuatoriana y afrodescendiente....',
+	'ALEX: ¿Qué una llave? Si vi una por aquí arriba en los antiguos molinos...',
+	'ALEX: Tiene muros de ladrillo y una bandera roja en la puerta.',
 ]
 
 const TEXT_SOFIA: Array =[
-	'SOFIA: Texto de prueba linea 1',
-	'SOFIA: Texto de prueba linea 2',
+	'SOFIA: Tu abuelo es un gran agricultor, me ayudo mucho con mis vacas...',
+	'SOFIA: ¿Sabías que? Los apellidos de origen africano...',
+	'SOFIA: Que hasta hoy se mantienen en las familias afroecuatorianas...',
+	'SOFIA: Son el testimonio vivo e histórico que los identifica...',
+	'SOFIA: Y revela el lugar de donde provienen sus ancestros...',
+	'SOFIA: Por ejemplo, tu abuelo se llama Salomón Congo.',
+	'SOFIA: La llave que buscas esta en el laberinto aqui a lado...',
+	'SOFIA: Tu abuelo me pidió que la escondiera y no se me ocurrió otro lugar.',
 ]
 
+
 const TEXT_DIEGO: Array =[
-	'DIEGO: Texto de prueba linea 1',
-	'DIEGO: Texto de prueba linea 2',
+	'DIEGO: ¿Sabías que...? A finales del siglo XVII...',
+	'DIEGO: Llegaron los primeros africanos a la zona del Chota...',
+	'DIEGO: Traídos como esclavos para trabajar en las haciendas jesuitas...',
+	'DIEGO: Entonces se da el primer choque cultural...',
+	'DIEGO: Al interior de esta comunidad...',
+	'DIEGO: Su pasado se vuelve presente y se plasma...',
+	'DIEGO: Construyeron  instrumentos similares a los que utilizaban en África...',
+	'DIEGO: Así revivieron su historia musica...',
+	'DIEGO: El cántico y la percusión, en el caso de la bomba.',
+	'DIEGO: ¿Una llave? Si Alex dijo algo sobre una llave...',
+	'DIEGO: Sigue subiendo, quizá lo encuentres...',
 ]
 
 const TEXT_KEVIN: Array =[
-	'KEVIN: Texto de prueba linea 1',
-	'KEVIN: Texto de prueba linea 2',
+	'KEVIN: Sabías que nuestros ancestros africanos y sus descendientes...',
+	'KEVIN: contaron y hasta hoy cuentan las historias de sus comunidades...',
+	'KEVIN: y sus saberes a través de los cuentos...',
+	'KEVIN: mitos, canciones, juegos y leyendas?',
+	'KEVIN: La leyenda de la Tunda es una de las más conocidas en Esmeraldas...',
+	'KEVIN: Mito narrado a niños y jóvenes para inculcarles valores éticos',
+	'KEVIN: Ten cuidado al cruzar el puente, no se te vaya a aparecer la Tundra...',
 ]
 
 const TEXT_MARIA: Array =[
-	'MARIA: Texto de prueba linea 1',
-	'MARIA: Texto de prueba linea 2',
+	'MARIA: ¿Sabías que? Las comunidades afroecuatorianas...',
+	'MARIA: Se caracterizan por el consumo de productos del medio...',
+	'MARIA: En El Chota, La Concepción y Salinas yuca, plátano...',
+	'MARIA: Caña de azúcar, fréjol, guandul, camote, entre otros...',
+	'MARIA: Son la base de la alimentación de la población.',
+	'MARIA: En los territorios de Esmeraldas la alimentación...',
+	'MARIA: Se basa en el consumo de productos de los ríos y del mar.',
 ]
 
 const TEXT_CARLOS: Array =[
-	'CARLOS: Texto de prueba linea 1',
-	'CARLOS: Texto de prueba linea 2',
+	'CARLOS: Canto, canto, río, río. Pero más río, que canto...',
+	'CARLOS: ¿Por qué si mucho canto, es poco lo que río?',
+	'CARLOS: ¿Qué cosa es?',
+	'CARLOS: Si el río ja ja, ¿Sabías que...? ',
+	'CARLOS: En las comunidades y caseríos donde habitan afroecuatorianos...',
+	'CARLOS: Los ríos tienen un valor que va más allá del agua que usamos ',
+	'CARLOS: Al otro lado del río vive JUAN él debe tener una llave.',
 ]
 
 var chats: int = 0
@@ -172,6 +229,10 @@ func _on_carlos_body_entered(body):
 		set_dialog(TEXT_CARLOS[chats],NPCS.carlos,IMG['CARLOS'])
 		chats += 1
 
+#func _input(event: InputEvent) -> void:
+#	if Input.is_action_pressed("continue"):
+#		_on_Button_pressed()
+
 func _on_Button_pressed():
 	match npc:
 		NPCS.chest:
@@ -189,7 +250,7 @@ func _on_Button_pressed():
 				chats += 1
 			else:
 				#dialogue ended
-				chats -= TEXT_CHEST.size()
+				chats -= TEXT_CHEST2.size()
 				hide()
 				get_tree().paused = false
 				get_tree().change_scene("res://Scenes/winnerUI.tscn")
@@ -249,7 +310,7 @@ func _on_Button_pressed():
 				get_tree().paused = false 
 		NPCS.sofia:
 			if chats < TEXT_SOFIA.size():
-				set_dialog(TEXT_SOFIA[chats],NPCS.alex, IMG['SOFIA'])
+				set_dialog(TEXT_SOFIA[chats],NPCS.sofia, IMG['SOFIA'])
 				chats += 1
 			else:
 				#dialogue ended
@@ -266,7 +327,7 @@ func _on_Button_pressed():
 				hide()
 				get_tree().paused = false
 		NPCS.kevin:
-			if chats < TEXT_DIEGO.size():
+			if chats < TEXT_KEVIN.size():
 				set_dialog(TEXT_KEVIN[chats],NPCS.kevin, IMG['KEVIN'])
 				chats += 1
 			else:
